e320cfb8306fac85b723c3e1a5a3c44d93a379fe
Tekle Gebreyohannes <tgyohannes@excellerentsolutions.com>
2022-10-12 11:25:09 +0300

    Add morgan & helmet 3rd party middlewares

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const genresRouter = require('./src/routes/router.genres');
 
 const PORT = process.env.PORT || 5000;
+
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan is enabled...');
+}
 
 app.use('/api/genres', genresRouter);
 
