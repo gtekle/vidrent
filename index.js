d79444a993deef85da882bcd30cc71e12f81330a
Tekle Gebreyohannes <tgyohannes@excellerentsolutions.com>
2022-10-12 11:03:35 +0300

    Add urlencoded and static built-in middlewares

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.use('/api/genres', genresRouter);
 
