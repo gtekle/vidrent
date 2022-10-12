165fe37e66c461c8f0e855281d55c6f3db986cdd
Tekle Gebreyohannes <tgyohannes@excellerentsolutions.com>
2022-10-12 11:56:46 +0300

    Add environment specific configs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 
 const genresRouter = require('./src/routes/router.genres');
 
@@ -12,6 +13,10 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+console.log('Application Name: ', config.get('name'));
+console.log('Mail Server: ', config.get('mail.host'));
+console.log('Mail Password: ', config.get('mail.password'));
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
