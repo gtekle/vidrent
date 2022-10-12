dd6d1a6d5778b74417babb602c1ca7a315cb9d66
Tekle Gebreyohannes <tgyohannes@excellerentsolutions.com>
2022-10-12 12:31:10 +0300

    Add debug config with env variables

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -9,20 +12,23 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Middlewares
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-console.log('Application Name: ', config.get('name'));
-console.log('Mail Server: ', config.get('mail.host'));
-console.log('Mail Password: ', config.get('mail.password'));
+startupDebugger('Application Name: ', config.get('name'));
+startupDebugger('Mail Password: ', config.get('mail.password'));
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan is enabled...');
+    startupDebugger('Morgan is enabled...');
 }
 
+dbDebugger('Database connected...');
+
+// Routes
 app.use('/api/genres', genresRouter);
 
 
